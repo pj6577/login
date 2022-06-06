608ea667c1df829e692793fc0e0703a7847ab7f1
JAEHYEON PARK <pj6577@naver.com>
2022-06-06 19:01:43 +0900

    dom 이용 로그인 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,5 +1,17 @@
 "use strict";
 
 
-console.log("gi");
-console.log("gi2");
+let id = document.querySelector("#id"),
+psword = document.querySelector("#psword"),
+loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id : id.value,
+        psword : psword.value,
+    };
+    
+}
+
