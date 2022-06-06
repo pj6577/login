27973c467ad75cef4d1695f9fdfb142738b6086f
JAEHYEON PARK <pj6577@naver.com>
2022-06-06 19:10:21 +0900

    fetch 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -2,16 +2,23 @@
 
 
 let id = document.querySelector("#id"),
-psword = document.querySelector("#psword"),
-loginBtn = document.querySelector("button");
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
 
 loginBtn.addEventListener("click", login);
 
-function login(){
+function login() {
     const req = {
-        id : id.value,
-        psword : psword.value,
+        id: id.value,
+        psword: psword.value,
     };
-    
+
+    fetch("/login", {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    });
 }
 
