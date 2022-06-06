dd839db85d72014cbe99fc5045e14a9d01b75361
JAEHYEON PARK <pj6577@naver.com>
2022-06-06 17:40:17 +0900

    폴더구조 바꾸기

---
+++ app/app.js
@@ -0,0 +1,22 @@
+
+
+//모듈
+const express = require("express");
+const app = express();
+
+
+//라우팅
+const home = require("./src/routers/home"); 
+
+const path = require('path'); 
+app.set('view engine', 'ejs'); //'ejs'탬플릿을 엔진으로 한다.
+app.set('views', path.join(__dirname, "./src/views")); //폴더, 폴더경로 지정
+
+
+app.use("/", home);  //use  - > 미들 웨어를 등록해주는 메서드.
+
+
+
+module.exports = app;
+
+// package.json
---
+++ app/bin/www.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const app = require("../app");
+const PORT = 3001;
+app.listen(PORT,  ()=> {
+    console.log("서버가동");
+});
