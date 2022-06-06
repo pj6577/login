2068419eb40eeb891f68ac7fbb6cb065ee550eca
JAEHYEON PARK <pj6577@naver.com>
2022-06-06 20:09:35 +0900

    로그인 구현

--- app/app.js
+++ app/app.js
@@ -2,7 +2,9 @@
 
 //모듈
 const express = require("express");
+const bodyParser = require("body-Parser");
 const app = express();
+
 
 
 //라우팅
@@ -12,6 +14,10 @@ const path = require('path');
 app.set('view engine', 'ejs'); //'ejs'탬플릿을 엔진으로 한다.
 app.set('views', path.join(__dirname, "./src/views")); //폴더, 폴더경로 지정
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+
+//url을 통해 전달되는 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제를 해결 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", home);  //use  - > 미들 웨어를 등록해주는 메서드.
 
--- app/src/routers/home/home.ctrl.js
+++ app/src/routers/home/home.ctrl.js
@@ -1,15 +1,46 @@
 "use strict";
 
-const home = (req, res) => {
-    res.render("home/index");
+
+
+const output = {
+    home : (req, res) => {
+        res.render("home/index");
+    },
+    
+     login : (req, res) => {
+        res.render("home/login");
+    },
+
 };
 
-const login = (req, res) => {
-    res.render("home/login");
-}
+const users = {
+    id: ["박재현", "테스트1", "테스트2"],
+    psword : ["1234", "12345", "12345"],
+};
+
+const process = {
+    login : (req, res) =>{
+        const id = req.body.id,
+        psword = req.body.psword;
+        
+        if(users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword){
+                return res.json({
+                    success : true,
+                    msg : "로그인에 성공하셨습니다",
+                });
+            }
+        }
+        return res.json({
+            success : false,
+            msg : "로그인에 실패하셧습니다",
+        });
+    },
+};
 
 module.exports = {
-    home,
-    login,
+  output,
+  process,
 };
 
